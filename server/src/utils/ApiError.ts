ce1bc5e0bd5564c30cc56f3a2bad5f88cfc0c3c1
Ayush Karma <ayushkarma.dev@gmail.com>
2026-06-25 12:35:01 +0530

    feat: implement ApiError class for standardized error handling

---
+++ server/src/utils/ApiError.ts
@@ -0,0 +1,19 @@
+class ApiError extends Error {
+  statusCode: number;
+  errors: unknown[];
+
+  constructor(
+    statusCode: number,
+    message = "Something went wrong",
+    errors: unknown[] = [],
+  ) {
+    super(message);
+
+    this.statusCode = statusCode;
+    this.errors = errors;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export default ApiError;
