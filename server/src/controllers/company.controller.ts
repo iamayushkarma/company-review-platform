4b94120290ca129eaae8e10d1d80a6b384a0cdb3
Ayush Karma <ayushkarma.dev@gmail.com>
2026-06-25 12:55:21 +0530

    feat: implement company controller and service with CRUD operations

---
+++ server/src/controllers/company.controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import asyncHandler from "../utils/asyncHandler";
+import ApiResponse from "../utils/ApiResponse";
+
+import * as companyService from "../services/company.service";
+
+export const createCompany = asyncHandler(async (req, res) => {
+  const company = await companyService.createCompany(req.body);
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, company, "Company created successfully"));
+});
+
+export const getCompanies = asyncHandler(async (req, res) => {
+  const companies = await companyService.getCompanies();
+
+  return res.json(new ApiResponse(200, companies));
+});
+
+export const getCompanyById = asyncHandler(async (req, res) => {
+  const company = await companyService.getCompanyById(req.params.id as string);
+
+  return res.json(new ApiResponse(200, company));
+});
+
+export const searchCompanies = asyncHandler(async (req, res) => {
+  const { search, city, sort } = req.query;
+
+  const companies = await companyService.searchCompanies(
+    search as string,
+    city as string,
+    sort as string,
+  );
+
+  return res.json(new ApiResponse(200, companies));
+});
