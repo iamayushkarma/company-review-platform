51731fe5baaed17eafc3a5361406c02449ded339
Ayush Karma <ayushkarma.dev@gmail.com>
2026-06-25 17:43:13 +0530

    feat: enhance AddReviewModal with rating selection and improved form layout

--- client/src/components/review/AddReviewModal.tsx
+++ client/src/components/review/AddReviewModal.tsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { X } from "lucide-react";
+import { Star, X } from "lucide-react";
+
+import Modal from "../common/Modal";
+import { Input } from "../common/Input";
+import { Button } from "../common/Button";
 
 import { createReview } from "../../api/review.api";
 
@@ -18,6 +23,7 @@ interface ReviewForm {
 }
 
 const AddReviewModal = ({ open, companyId, onClose, onSuccess }: Props) => {
+  const [rating, setRating] = useState(0);
   const {
     register,
     handleSubmit,
@@ -32,7 +38,7 @@ const AddReviewModal = ({ open, companyId, onClose, onSuccess }: Props) => {
       await createReview({
         ...data,
         company: companyId,
-        rating: Number(data.rating),
+        rating: rating,
       });
 
       reset();
@@ -46,64 +52,118 @@ const AddReviewModal = ({ open, companyId, onClose, onSuccess }: Props) => {
   };
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 p-5">
-      <div className="w-full max-w-lg rounded-xl bg-white">
-        <div className="flex items-center justify-between border-b p-5">
-          <h2 className="text-xl font-semibold">Add Review</h2>
+    <Modal isOpen={open} onClose={onClose} className="max-w-xl">
+      <div className="relative overflow-hidden rounded-2xl bg-white px-8 py-10">
+        {/* Decorations */}
 
-          <button onClick={onClose}>
-            <X />
-          </button>
-        </div>
+        <div className="absolute -left-8 -top-8 z-20 h-28 w-28 rounded-full bg-gradient-to-br from-fuchsia-500 to-indigo-700"></div>
 
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-5 p-6">
-          <input
-            {...register("fullName", { required: true })}
-            placeholder="Full Name"
-            className="w-full rounded-lg border p-3"
+        <div className="absolute left-12 -top-8 z-10 h-24 w-24 rounded-full bg-violet-200"></div>
+
+        {/* Close */}
+
+        <button
+          onClick={onClose}
+          className="absolute right-6 top-6 cursor-pointer"
+        >
+          <X size={24} />
+        </button>
+
+        {/* Title */}
+
+        <h2 className="mb-10 text-center text-2xl font-semibold">Add Review</h2>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+          <Input
+            label="Full Name"
+            placeholder="Enter"
+            {...register("fullName", {
+              required: true,
+            })}
           />
 
-          <input
-            {...register("subject", { required: true })}
-            placeholder="Subject"
-            className="w-full rounded-lg border p-3"
+          <Input
+            label="Subject"
+            placeholder="Enter"
+            {...register("subject", {
+              required: true,
+            })}
           />
 
-          <textarea
-            rows={5}
-            {...register("review", { required: true })}
-            placeholder="Write your review..."
-            className="w-full rounded-lg border p-3"
-          />
+          <div>
+            <label className="mb-2 block text-sm font-medium text-gray-500">
+              Enter your Review
+            </label>
 
-          <input
-            type="number"
-            min={1}
-            max={5}
-            {...register("rating", { required: true })}
-            placeholder="Rating (1-5)"
-            className="w-full rounded-lg border p-3"
-          />
+            <textarea
+              rows={5}
+              placeholder="Description"
+              {...register("review", {
+                required: true,
+              })}
+              className="
+              w-full
+              resize-none
+              rounded-lg
+              border
+              border-border-subtle
+              px-3
+              py-3
+              text-sm
+              outline-none
+              transition-colors
+              focus:border-2
+              focus:border-gray-700
+          "
+            />
+          </div>
 
-          <div className="flex justify-end gap-3">
-            <button
-              type="button"
-              onClick={onClose}
-              className="rounded-lg border px-5 py-2"
-            >
-              Cancel
-            </button>
+          {/* Rating */}
 
-            <button
-              disabled={isSubmitting}
-              className="rounded-lg bg-violet-600 px-5 py-2 text-white"
-            >
-              {isSubmitting ? "Submitting..." : "Submit"}
-            </button>
+          <div className="pt-2">
+            <h3 className="mb-4 text-lg font-semibold">Rating</h3>
+
+            <div className="flex items-center justify-between">
+              <div className="flex gap-2">
+                {[1, 2, 3, 4, 5].map((star) => (
+                  <button
+                    key={star}
+                    type="button"
+                    onClick={() => setRating(star)}
+                    className="cursor-pointer"
+                  >
+                    <Star
+                      size={32}
+                      className={
+                        star <= rating
+                          ? "fill-[#F4B400] text-[#F4B400]"
+                          : "fill-gray-300 text-gray-300"
+                      }
+                    />
+                  </button>
+                ))}
+              </div>
+
+              <span className="text-md text-gray-500">
+                {rating >= 4
+                  ? "Satisfied"
+                  : rating >= 2
+                    ? "Average"
+                    : rating
+                      ? "Unsatisfied"
+                      : ""}
+              </span>
+            </div>
+          </div>
+
+          <div className="flex justify-center pt-3">
+            <Button loading={isSubmitting} type="submit" className="min-w-36">
+              Save
+            </Button>
           </div>
         </form>
       </div>
-    </div>
+    </Modal>
   );
 };
 
