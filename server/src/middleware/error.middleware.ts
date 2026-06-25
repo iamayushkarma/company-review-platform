bb384fe1b65009abc295b7a06f162dc8e789ef75
Ayush Karma <ayushkarma.dev@gmail.com>
2026-06-25 12:36:09 +0530

    feat: add error handling middleware for standardized error responses

---
+++ server/src/middleware/error.middleware.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import ApiError from "../utils/ApiError";
+
+const errorHandler = (
+  err: ApiError,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  res.status(err.statusCode || 500).json({
+    success: false,
+    message: err.message,
+    errors: err.errors || [],
+  });
+};
+
+export default errorHandler;
