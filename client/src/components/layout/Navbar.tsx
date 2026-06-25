59038b0d1ba1749f0c6651ed90b01e98a868c6f7
Ayush Karma <ayushkarma.dev@gmail.com>
2026-06-25 18:13:38 +0530

    feat: make logo clickable to navigate to home in Navbar component

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -1,9 +1,11 @@
 import { Star } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const navigate = useNavigate();
   return (
     <header className="h-16 shadow-md flex items-center justify-between">
-      <div className="mx-20">
+      <div className="mx-20 cursor-pointer" onClick={() => navigate("/")}>
         {/* Logo */}
         <div className="flex items-center justify-center gap-2">
           <div className="size-10 rounded-full bg-primary flex text-white items-center justify-center">
