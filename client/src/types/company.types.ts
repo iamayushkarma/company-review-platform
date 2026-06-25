68ea7d1f71c76cacfc443128fd8d2f4c848162e8
Ayush Karma <ayushkarma.dev@gmail.com>
2026-06-25 13:36:32 +0530

    feat: define Company and Review interfaces in type definitions

---
+++ client/src/types/company.types.ts
@@ -0,0 +1,13 @@
+export interface Company {
+  _id: string;
+  name: string;
+  logo: string;
+  foundedOn: string;
+  city: string;
+  location: string;
+  description: string;
+  rating: number;
+  reviewCount: number;
+  createdAt: string;
+  updatedAt: string;
+}
---
+++ client/src/types/review.types.ts
@@ -0,0 +1,10 @@
+export interface Review {
+  _id: string;
+  company: string;
+  fullName: string;
+  subject: string;
+  review: string;
+  rating: number;
+  likes: number;
+  createdAt: string;
+}
